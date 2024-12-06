67722cff9fe23bd789ad1be933f206a4671500d5
ngoclamph <ngoclam.pham06@gmail.com>
2024-12-05 21:52:51 -0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -2,10 +2,8 @@
  * Copyright (c) 2024 Your Name
  * SPDX-License-Identifier: Apache-2.0
  */
-
 `default_nettype none
-
-module tt_um_example (
+module tt_um_lampham_prbs31 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
