4651258d2eb0d9addc19a6d4b881bb475731ab69
ngoclamph <ngoclam.pham06@gmail.com>
2025-01-24 22:25:23 -0800

    Update project.v
    plz work

--- src/project.v
+++ src/project.v
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 `default_nettype none
-module tt_um_davidparent_prbs31 (
+module tt_um_lampham_prbs31 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
