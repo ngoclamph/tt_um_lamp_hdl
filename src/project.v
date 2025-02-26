79366e3c562424cbbaaacf3cbdb5937a01a91c88
ngoclamph <ngoclam.pham06@gmail.com>
2025-02-26 15:19:37 -0800

    Update project.v
    Sample run

--- src/project.v
+++ src/project.v
@@ -4,30 +4,61 @@
  */
 `default_nettype none
 module tt_um_lampham_prbs31 (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+  input  wire [7:0] ui_in,    // Dedicated inputs
+  output wire [7:0] uo_out,   // Dedicated outputs
+  input  wire [7:0] uio_in,   // IOs: Input path
+  output wire [7:0] uio_out,  // IOs: Output path
+  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+  input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+  input  wire       clk,      // clock
+  input  wire       rst_n     // reset_n - low to reset
 );
-    reg [30:0] lfsr; 
-    always @(posedge clk or posedge rst_n) begin
-        if (rst_n) begin
-        lfsr <= 31'd1; // Reset counter
-    end else begin
-        // Increment counter on each clock cycle
-        lfsr[0] <= lfsr[27] ^ lfsr[30] ;
-        lfsr[30:1] <=lfsr[29:0] ;  
-    end
-end  
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out[0] =lfsr[30] ;
+
+  // VGA signals
+  wire hsync;
+  wire vsync;
+  wire [1:0] R;
+  wire [1:0] G;
+  wire [1:0] B;
+  wire video_active;
+  wire [9:0] pix_x;
+  wire [9:0] pix_y;
+  wire sound;
+
+  // TinyVGA PMOD
+  assign uo_out = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
+
+  // Unused outputs assigned to 0.
   assign uio_out = 0;
   assign uio_oe  = 0;
-  assign uo_out[7:1]= 7'd0;
-  // List all unused inputs to prevent warnings
-    wire _unused = &{ena, uio_in, ui_in, 1'b0}; 
+
+  // Suppress unused signals warning
+  wire _unused_ok = &{ena, ui_in, uio_in};
+
+  reg [9:0] counter;
+
+  hvsync_generator hvsync_gen(
+    .clk(clk),
+    .reset(~rst_n),
+    .hsync(hsync),
+    .vsync(vsync),
+    .display_on(video_active),
+    .hpos(pix_x),
+    .vpos(pix_y)
+  );
+  
+  wire [9:0] moving_x = pix_x + counter;
+
+  assign R = video_active ? {moving_x[5], pix_y[2]} : 2'b00;
+  assign G = video_active ? {moving_x[6], pix_y[2]} : 2'b00;
+  assign B = video_active ? {moving_x[7], pix_y[5]} : 2'b00;
+  
+  always @(posedge vsync) begin
+    if (~rst_n) begin
+      counter <= 0;
+    end else begin
+      counter <= counter + 1;
+    end
+  end
+  
 endmodule
